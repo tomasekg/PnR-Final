a8a513267796e32db35eda31ebbcc3c6cd26b4fc
tomasekg <BaconBits2>
2018-10-24 15:03:12 -0400

    fixed skill test

--- student.py
+++ student.py
@@ -78,8 +78,10 @@ class Piggy(pigo.Pigo):
                     # add up the numbers to right_total
                     right_total += self.scan[angle]
             # loop from self.MIDPOINT to self.MIDPOINT + 60
+            for angle in range(self.MIDPOINT, self.MIDPOINT + 60):
+                if self.scan[angle]:
                 # add up the numbers to left_total
-                left_total += self.scan[angle]
+                    left_total += self.scan[angle]
             # if right is bigger:
             if right_total > left_total:
                 # turn right
