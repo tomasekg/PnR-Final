c1732f83319f61bf2a14f837a7592e87666a3f9a
tomasekg <BaconBits2>
2018-11-05 15:00:34 -0500

    changed midpoint

--- student.py
+++ student.py
@@ -17,7 +17,7 @@ class Piggy(pigo.Pigo):
         """The robot's constructor: sets variables and runs menu loop"""
         print("I have been instantiated!")
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
-        self.MIDPOINT = 74
+        self.MIDPOINT = 78
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
         self.SAFE_STOP_DIST = 25
         # I changed the safe stop distance to a little higher just to test out
