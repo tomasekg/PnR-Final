1f243e267a07ecec5a9aa10a3d3ebe9b0bc3d845
tomasekg <BaconBits2>
2018-11-30 14:41:47 -0500

    changed safe stop dist

--- student.py
+++ student.py
@@ -19,7 +19,7 @@ class Piggy(pigo.Pigo):
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 87
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
-        self.SAFE_STOP_DIST = 32
+        self.SAFE_STOP_DIST = 30
         # I changed the safe stop distance to a little higher just to test out
         # tried changing again
         # lowered it back to what i had
